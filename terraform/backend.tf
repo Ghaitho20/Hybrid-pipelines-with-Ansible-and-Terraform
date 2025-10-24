a8eba5f2b1a48b7457e705b5ac1e6dfab3a52f51
ghaitho20 <ghaith.benammar709@gmail.com>
2025-10-25 00:06:45 +0200

    fix terraform code

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,8 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-cicd-tfstate"
-    storage_account_name = "stcicdtfstate"   
-    container_name       = "tfstate"
-    key                  = "cicd/terraform.tfstate"
-  }
-}
-
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name  = "rg-cicd-tfstate"
+#     storage_account_name = "stcicdtfstate"   
+#     container_name       = "tfstate"
+#     key                  = "cicd/terraform.tfstate"
+#   }
+# }
