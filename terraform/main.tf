a7f4d7676b5b78d0a6c33b32d381255cb4131d2d
ghaitho20 <ghaith.benammar709@gmail.com>
2025-10-25 20:05:58 +0000

    fix jenkins file

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,8 +47,6 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-
-  # Add this rule for port 5000
   security_rule {
     name                       = "BACKEND"
     priority                   = 1003
@@ -60,6 +58,18 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+  security_rule {
+  name                       = "GRAFANA"
+  priority                   = 1004
+  direction                  = "Inbound"
+  access                     = "Allow"
+  protocol                   = "Tcp"
+  source_port_range          = "*"
+  destination_port_range     = "3000"
+  source_address_prefix      = "*"
+  destination_address_prefix = "*"
+ }
+
 }
 
 
